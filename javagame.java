8bdbb9572ad728d3c96af0a81e0406a6fdf84ac6
the76000 <kyle.berkoski1@marist.edu>
2014-01-30 15:48:04 -0500

    Java Game
    First Java version due 1/30

---
+++ javagame.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class javagame {
+
+	  // Global
+	  public static int currentLocale = 1; //player start area.
+	  public static String command;  
+	  public static boolean done = false;
+	  
+	  public static void main(String[] args) {
+		 init();
+		 updateDisplay();
+
+		
+		  //game loop
+		 while(! done){
+		 getCommand();
+	     navigate();
+	     updateDisplay();
+		 }
+		 System.out.println("Thanks for playing!");
+	  }
+	  
+	  private static void init(){
+		  command = new String();
+	  }  
+	 
+
+	  private static void updateDisplay() {
+	     String msg = new String();
+	     msg = "";
+	     switch (currentLocale) {
+	        case 0: msg = "The hull";
+	                break;
+	        case 1: msg = "Main deck";
+            		break;
+	        case 2: msg = "Below Deck";
+            		break;
+	        default: msg = "currentLocale" + currentLocale + " does not compute.";
+	     }
+	     System.out.println(msg);
+	  } 
+	  
+	  private static void getCommand(){
+		  Scanner inputReader = new Scanner(System.in);
+		  command = inputReader.nextLine();
+
+	  }
+	  private static void navigate(){
+		  if (command.equals("north") || command.equals("n") ){
+			//north
+			  if (currentLocale == 1){
+				  currentLocale = 0;
+			  } else if (currentLocale == 2){
+				  currentLocale = 1;
+			  } else if (currentLocale == 0){
+				  System.out.println("You jumped off the boat. Good job.");
+			  }
+			  }
+		  if (command.equals("south") || command.equals("s") ){
+					//north
+					  if (currentLocale == 1){
+						  currentLocale = 0;
+					  } else if (currentLocale == 0){
+						  currentLocale = 2;
+					  }
+		  } else if ( command.equals("exit") || command.equals("quit")){
+			done = true; 
+		 }
+
+	  }
+
+}
+
